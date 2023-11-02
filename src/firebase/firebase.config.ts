8f6ff1e44dd6a352803a1db91ba522d362f4e8cf
Ibrohimbek-Dev <programming012@gmail.com>
2023-11-03 08:18:59 +0900

    first commit

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyB23zUCwnn3X6MjzAzqZb6SEACjjXVI4cU",
@@ -9,6 +9,7 @@ export const firebaseConfig = {
   storageBucket: "mazza-restaurant-aa375.appspot.com",
   messagingSenderId: "263444533908",
   appId: "1:263444533908:web:5a33cb834eaac346ed5a81",
+  measurementId: "G-L931GRM8NH"
 };
 
 const app = initializeApp(firebaseConfig);
