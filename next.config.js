6ac63d9d42fce7a12759fb64234e9855239176ef
Ibrohimbek-Dev <programming012@gmail.com>
2023-11-04 14:40:36 +0900

    added output in next config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
 	reactStrictMode: true,
 	images: {
 		domains: ["image.tmdb.org", "rb.gy", "images.unsplash.com", "firebasestorage.googleapis.com"],
-	},	
+	},
+	output: {
+    dir: 'out', // Set your desired output directory
+  },
 };
 
 module.exports = nextConfig;
